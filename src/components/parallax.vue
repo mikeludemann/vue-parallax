ea9a2bdbc9d742d2d143f7a1386af658cb177907
Mike Ludemann <mike-ludemann@hotmail.de>
2020-03-19 16:36:44 +0100

    Setup complete - vue-parallax

---
+++ src/components/parallax.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+
+	<section class="parallax">
+		<div class="parallax" :style="imageStyle">.</div>
+		<div class="content" :style="contentStyle">
+			<slot></slot>
+		</div>
+	</section>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'parallax',
+		props: {
+			image: {
+				type: String
+			}
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+			imageStyle: function(){
+				return {
+					minHeight: "calc(100vh)",
+					width: "100%",
+					backgroundAttachment: "fixed",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundImage: "url(" + this.image + ")"
+				};
+			},
+			contentStyle: function(){
+				return {
+					backgroundColor: "#fff",
+					color: "#000",
+					padding: "10px"
+				};
+			}
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.parallax {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <parallax image="https://cdn03.nintendo-europe.com/media/images/10_share_images/portals_3/H2x1_SuperMario_Hub_image1600w.jpg">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </parallax>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import parallax from '@/components/parallax.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    parallax
+  }
+}
+</script>
